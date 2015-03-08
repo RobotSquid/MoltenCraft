71a3f54e7c36dd2eb4304b080fb3d4ce9706d85b
RobotSquid <robotsquidgaming@gmail.com>
2015-03-08 18:46:57 +0200

    Framework done! Starting with items next

---
+++ src/main/java/com/robotsquid/moltencraft/client/gui/GuiConfiguration.java
@@ -0,0 +1,21 @@
+package com.robotsquid.moltencraft.client.gui;
+
+import com.robotsquid.moltencraft.handler.ConfigurationHandler;
+import com.robotsquid.moltencraft.reference.Reference;
+import cpw.mods.fml.client.config.GuiConfig;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.common.config.ConfigElement;
+import net.minecraftforge.common.config.Configuration;
+
+public class GuiConfiguration extends GuiConfig
+{
+    public GuiConfiguration(GuiScreen guiScreen)
+    {
+        super(guiScreen,
+                new ConfigElement(ConfigurationHandler.configuration.getCategory(Configuration.CATEGORY_GENERAL)).getChildElements(),
+                Reference.MOD_ID,
+                false,
+                false,
+                GuiConfig.getAbridgedConfigPath(ConfigurationHandler.configuration.toString()));
+    }
+}
---
+++ src/main/java/com/robotsquid/moltencraft/reference/ConfigurationReference.java
@@ -0,0 +1,8 @@
+package com.robotsquid.moltencraft.reference;
+
+public class ConfigurationReference
+{
+    public static boolean isAwesome = false;
+    public static boolean isActive = false;
+    public static boolean isHard = false;
+}
