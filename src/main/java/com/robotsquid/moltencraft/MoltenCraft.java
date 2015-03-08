f21ccf4dd9b6cbc6360d892411d79aca538e3061
RobotSquid <robotsquidgaming@gmail.com>
2015-03-08 18:49:31 +0200

    Added missing classes

---
+++ src/main/java/com/robotsquid/moltencraft/MoltenCraft.java
@@ -0,0 +1,42 @@
+package com.robotsquid.moltencraft;
+
+import com.robotsquid.moltencraft.handler.ConfigurationHandler;
+import com.robotsquid.moltencraft.proxy.IProxy;
+import com.robotsquid.moltencraft.reference.Reference;
+import com.robotsquid.moltencraft.utility.LogHelper;
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid=Reference.MOD_ID, name=Reference.MOD_NAME, version= Reference.VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
+public class MoltenCraft
+{
+    @Mod.Instance(Reference.MOD_ID)
+    public static MoltenCraft instance;
+
+    @SidedProxy(clientSide = Reference.SERVER_PROXY_CLASS, serverSide = Reference.CLIENT_PROXY_CLASS)
+    public static IProxy proxy;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        ConfigurationHandler.init(event.getSuggestedConfigurationFile());
+        FMLCommonHandler.instance().bus().register(new ConfigurationHandler());
+        LogHelper.info("Pre-Init Done!");
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        LogHelper.info("Init Done!");
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+        LogHelper.info("Post-Init Done!");
+    }
+}
---
+++ src/main/java/com/robotsquid/moltencraft/handler/ConfigurationHandler.java
@@ -0,0 +1,50 @@
+package com.robotsquid.moltencraft.handler;
+
+import com.robotsquid.moltencraft.reference.ConfigurationReference;
+import com.robotsquid.moltencraft.reference.Reference;
+import com.robotsquid.moltencraft.utility.LogHelper;
+import cpw.mods.fml.client.event.ConfigChangedEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.common.config.Configuration;
+
+import java.io.File;
+
+public class ConfigurationHandler
+{
+    public static Configuration configuration;
+
+    public static void init(File configFile)
+    {
+        // Initialize the config object
+        if (configuration == null)
+        {
+            configuration = new Configuration(configFile);
+            loadConfigurations();
+        }
+    }
+
+    private static void loadConfigurations()
+    {
+        ConfigurationReference.isAwesome = configuration.getBoolean("isAwesome", Configuration.CATEGORY_GENERAL, true, "Is this mod awesome??");
+        ConfigurationReference.isActive = configuration.getBoolean("isActive", Configuration.CATEGORY_GENERAL, true, "Do you want this mod to be active?");
+        ConfigurationReference.isHard = configuration.getBoolean("isHard", Configuration.CATEGORY_GENERAL, false, "Do you want to play with an extra hard mod?");
+
+        if (configuration.hasChanged())
+        {
+            configuration.save();
+        }
+
+        LogHelper.info("isAwesome: " + ConfigurationReference.isAwesome);
+        LogHelper.info("isActive: " + ConfigurationReference.isActive);
+        LogHelper.info("isHard: " + ConfigurationReference.isHard);
+    }
+
+    @SubscribeEvent
+    public void onConfigurationChangedEvent(ConfigChangedEvent.OnConfigChangedEvent event)
+    {
+        if (event.modID.equalsIgnoreCase(Reference.MOD_ID))
+        {
+            loadConfigurations();
+        }
+    }
+}
